fd862e92a488679629d19db3bbb80f0ae1a72639
bhavy-sharma <bhavymay18@gmail.com>
2025-10-12 17:53:42 +0530

    All Links Connected

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
 import StatSection from '@/components/home/StatsSection'    //  Design Change krna hai iska Abhi
-import AboutSection from '@/components/home/AboutSection'    //  Design Change krna hai iska Abhi
+import Event from '@/components/home/Events'    //  Design Change krna hai iska Abhi
 import Carousel from '@/components/home/Carousel'
+import MediaGallery from '@/components/home/MediaGallery'
 
 const page = () => {
   return (
@@ -11,8 +12,9 @@ const page = () => {
       <Header/>
       <Carousel/>
       <StatSection/>
-      {/* <AboutSection/> */}
-      {/* <Footer/> */}
+      <Event/>
+      <MediaGallery/>
+      <Footer/>
     </div>
   )
 }
