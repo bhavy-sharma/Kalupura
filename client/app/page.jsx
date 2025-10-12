55ea5bce32febda467e647c7c8214d15365f7965
bhavy-sharma <bhavymay18@gmail.com>
2025-10-12 17:26:01 +0530

    Carousle Initialise

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
-import HeroSection from '@/components/home/HeroSection'    //  Design Change krna hai iska Abhi
 import StatSection from '@/components/home/StatsSection'    //  Design Change krna hai iska Abhi
 import AboutSection from '@/components/home/AboutSection'    //  Design Change krna hai iska Abhi
+import Carousel from '@/components/home/Carousel'
 
 const page = () => {
   return (
     <div>
       <Header/>
-      <HeroSection/>
+      <Carousel/>
       <StatSection/>
-      <AboutSection/>
-      <Footer/>
+      {/* <AboutSection/> */}
+      {/* <Footer/> */}
     </div>
   )
 }
