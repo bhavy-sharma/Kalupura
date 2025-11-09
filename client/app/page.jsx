96cc20a6510c50274e0bcd46ed957b7fb15dfa32
bhavy-sharma <bhavymay18@gmail.com>
2025-11-09 14:31:30 +0530

    Birthday Component loaded

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
-import StatSection from '@/components/home/StatsSection'    //  Design Change krna hai iska Abhi
-import Event from '@/components/home/Events'    //  Design Change krna hai iska Abhi
+import StatSection from '@/components/home/StatsSection'
+import Event from '@/components/home/Events'
 import Carousel from '@/components/home/Carousel'
 import MediaGallery from '@/components/home/MediaGallery'
 import SearchFilter from '@/components/home/SearchFilter'
+import BirthdaySection from '@/components/home/BirthdaySection'
 
 const page = () => {
   return (
     <div>
       <Header/>
       <Carousel/>
+      <BirthdaySection/>
       <SearchFilter/>
       <StatSection/>
       <Event/>
